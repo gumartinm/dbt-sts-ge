1eeeed3040163c6907e3148b7054c3e8d5c8f45b
Gustavo Martin Morcuende <gu.martinm@gmail.com>
2021-05-18 00:01:32 +0200

    Using great expectations

--- dags/example_dag.py
+++ dags/example_dag.py
@@ -1,9 +1,11 @@
 from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
 from datetime import datetime
 from airflow_dbt.operators.dbt_operator import (
     DbtSeedOperator,
     DbtRunOperator
 )
+from great_expectations_provider.operators.great_expectations import GreatExpectationsOperator
 import os
 
 
@@ -13,10 +15,12 @@ default_args = {
     'start_date': datetime(2020, 5, 21)
 }
 
-PROJECT_HOME = '/opt/airflow/dbt'
-DBT_PROJECT_DIR = os.path.join(PROJECT_HOME, 'sts')
+PROJECT_HOME = '/opt/airflow'
+DBT_ROOT_DIR = os.path.join(PROJECT_HOME, 'dbt')
+DBT_PROJECT_DIR = os.path.join(DBT_ROOT_DIR, 'sts')
 DBT_TARGET = 'dev'
 DBT_TARGET_DIR = os.path.join(DBT_PROJECT_DIR, 'target')
+GE_ROOT_DIR = os.path.join(PROJECT_HOME, 'great_expectations')
 
 dag = DAG(
     dag_id='example_dag',
@@ -29,8 +33,26 @@ dag = DAG(
 dbt_seed = DbtSeedOperator(
     task_id='dbt_seed',
     dir=DBT_PROJECT_DIR,
-    profiles_dir=PROJECT_HOME,
+    profiles_dir=DBT_ROOT_DIR,
     target=DBT_TARGET,
+    dag=dag
+)
+
+
+validate_load = GreatExpectationsOperator(
+    task_id='validate_load',
+    assets_to_validate=[
+        {
+            'batch_kwargs': {
+                'datasource': 'spark-thrift-server',
+                'schema': 'example',
+                'table': 'taxi_zone_lookup',
+                'data_asset_name': 'taxi_zone_lookup'
+            },
+            'expectation_suite_name': 'custom_sql_query.warning'
+        }
+    ],
+    data_context_root_dir=GE_ROOT_DIR,
     dag=dag
 )
 
@@ -38,10 +60,16 @@ dbt_seed = DbtSeedOperator(
 dbt_run = DbtRunOperator(
     task_id='dbt_run',
     dir=DBT_PROJECT_DIR,
-    profiles_dir=PROJECT_HOME,
+    profiles_dir=DBT_ROOT_DIR,
     target=DBT_TARGET,
     dag=dag
 )
 
+ge_docs_generate = BashOperator(
+    task_id='ge_docs_generate',
+    bash_command=f'great_expectations docs build --directory {GE_ROOT_DIR} --assume-yes',
+    dag=dag
+)
 
-dbt_seed >> dbt_run
+
+dbt_seed >> validate_load >> ge_docs_generate >> dbt_run
